58312b12df7a687a0cfdb94d4ffe7ac3612518a4
Murtaza Hussain <murtaza32004@gmail.com>
2026-04-16 01:37:50 -0700

    Ensure MLflow experiment is created if missing
    Add check to create experiment if it doesn't exist

--- src/utils/mlflow_config.py
+++ src/utils/mlflow_config.py
@@ -5,3 +5,8 @@ def setup_mlflow(experiment_name="FER-Emotion-Recognition"):
     mlflow.set_tracking_uri("sqlite:///mlflow.db")
 
     experiment = mlflow.get_experiment_by_name(experiment_name)
+
+    if experiment is None:
+        mlflow.create_experiment(experiment_name)
+
+    mlflow.set_experiment(experiment_name)
