5c3272d3f299a425055cf01383fc1001b4fee429
Murtaza Hussain <murtaza32004@gmail.com>
2026-04-19 22:58:29 -0700

    Delete src/tests directory

--- src/tests/test_api.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from api.app import app
-
-client = TestClient(app)
-
-def test_predict_endpoint():
-    response = client.post("/predict", json={"image": "dummy_data"})
-
-    assert response.status_code in [200, 400]
